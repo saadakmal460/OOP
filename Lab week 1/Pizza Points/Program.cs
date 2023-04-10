6e1ca7b316e080a9695fa5607ec48793e4f7f015
Saad@DESKTOP-UBGD9AO <saadakmal460@gmail.comgit config --global user.email saadakmal460@gmail.com>
2023-04-10 22:35:10 +0500

    OOP Tasks

--- Lab week 1/Pizza Points/Program.cs
+++ Lab week 1/Pizza Points/Program.cs
@@ -11,150 +11,88 @@ namespace Pizza_Points
     {
         static void Main(string[] args)
         {
+            int price, orders;
             string path = "D:\\2nd Semester\\OOP\\Lab week 1\\Pizza Points\\customers.txt";
-            string[] names = new string[100];
-            int[] orders = new int[100];
-            int[][] price = new int[100][];
-            int count = Load(path, names, orders, price);
+            Console.WriteLine("Enter number of orders: ");
+            orders = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < count; i++)
+            Console.WriteLine("Enter minimum price: ");
+            price = int.Parse(Console.ReadLine());
+
+            string result = Parsing(path, orders, price);
+            for (int i = 0; i < result.Length; i++)
             {
-                Console.Write(names[i]);
-                Console.Write(orders[i]);
-                Console.Write(price[i]);
-            }
-            while (true)
-            {
-                int option = 0;
-                option = Menu();
-                while (option != 3)
+                if (result[i] != ',')
                 {
-                    if (option == 1)
-                    {
-                        Console.Clear();
-                        AddUser();
-                        break;
-                    }
-
-
+                    Console.Write(result[i]);
+                    
+                }
+                else
+                {
+                    Console.WriteLine(" ");
                 }
             }
             Console.ReadKey();
         }
 
 
-
-
-
-
-
-
-        static string Parsing(string record, int field)
+        static string Parsing(string path , int orders , int price)
         {
-            int count = 1;
             string item = "";
-            for (int i = 0; i < record.Length; i++)
+            string record;
+            StreamReader file = new StreamReader(path);
+            while ((record = file.ReadLine()) != null)
             {
-                if (record[i] == ' ' || record[i] == '[')
+                string name = "";
+                for (int i = 0; record[i] != ' '; i++)
                 {
-                    count++;
+                    name = name + record[i];
                 }
-                else if (count == field)
+                if (LargestNumber(record, orders, price))
                 {
-                    item = item + record[i];
+                    item = item + name + ",";
                 }
             }
+
+            file.Close();
             return item;
         }
 
-        static int Load(string path, string[] names, int[] orders, int[][] price)
+        static bool LargestNumber(string record, int orders, int price)
         {
+            bool flag = false;
             int count = 0;
-            string[] values = new string[100];
-            if (File.Exists(path))
+            int idx = -1;
+            string number = "";
+            for (int i = 0; i < record.Length; i++)
             {
-                StreamReader file = new StreamReader(path);
-                string record;
-                while ((record = file.ReadLine()) != null)
+                if (record[i] == '[')
                 {
-                    names[count] = Parsing(record, 1);
-                    orders[count] = int.Parse(Parsing(record, 2));
-                    values[count] = Parsing(record, 3);
-                    for (int i = 0; i < values.Length; i++)
-                    {
-                        price[count][i] = int.Parse(values[i]);
-                    }
-                    count++;
+                    idx = i + 1;
+                    break;
                 }
-                file.Close();
             }
-            return count;
-        }
-
-
-        static int Menu()
-        {
-            int option;
-            Console.WriteLine("\t1.Enter a new user");
-            Console.WriteLine("\t2.Customers eligible for awards");
-            Console.WriteLine("\t3.Exit");
-            Console.WriteLine("Ent option");
-            option = int.Parse(Console.ReadLine());
-            return option;
-        }
-        static void AddUser()
-        {
-            string path = "D:\\2nd Semester\\OOP\\Lab week 1\\Pizza Points\\customers.txt";
-            string name;
-            int orders;
-            int[] prices = new int[100];
-
-            Console.Write("Enter name: ");
-            name = Console.ReadLine();
-
-            Console.Write("Enter number of orders: ");
-            orders = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter prices: ");
-            for (int i = 0; i < orders; i++)
+            for (int i = idx; i < record.Length - 1; i++)
             {
-                prices[i] = int.Parse(Console.ReadLine());
-            }
-
-            Store(name , orders, prices, path);
-            Console.Clear();
-
-        }
-        static void Store(string name ,int orders , int[] prices , string path)
-        {
-            string o;
-       
-            string[] p = new string[100];
-            o = orders.ToString();
-            for (int i = 0; i < orders; i++)
-            {
-                p[i] = prices[i].ToString();
-            }
-            
-            StreamWriter file = new StreamWriter(path, true);
-
-            file.Write(name + " " + o + " " + "[");
-            for (int i = 0; i < orders; i++)
-            {
-                
-                if (i == orders - 1)
+                if (record[i] != ',')
                 {
-                    file.Write(p[i]);
+                    number = number + record[i];
                 }
                 else
                 {
-                    file.Write(p[i]);
-                    file.Write(",");
+                    if (int.Parse(number) >= price)
+                    {
+                        count++;
+                        number = "";
+                    }
+                    
                 }
             }
-            file.WriteLine("]");
-            file.Flush();
-            file.Close();
+            if (count >= orders)
+            {
+                flag = true;
+            }
+            return flag;
 
         }
     }
