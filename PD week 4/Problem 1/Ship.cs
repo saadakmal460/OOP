87cb69dfccd148d63044a0edfe994566646e2963
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-12 12:36:02 +0500

    OOP Tasks

---
+++ PD week 4/Problem 1/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Ship
+    {
+        public string shipNumber;
+        public string shipLattitude;
+        public string shipLongitude;
+
+        public Ship(string Number, string sl, string slog)
+        {
+            shipNumber = Number;
+            shipLattitude = sl;
+            shipLongitude = slog;
+        }
+
+    }
+}
