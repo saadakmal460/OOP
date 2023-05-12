2a53e0ef1d145c89393ae65bba2d8ec06a87b18f
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-12 12:32:04 +0500

    OOP Tasks

---
+++ Lab week 5/Self Assesment 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assesment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> x = new List<float>() { 1.2F, 5.6F, 8.9F, 34.876F };
+            List<string> name = new List<string>() { "saad", "ahmad", "dddf", "aa" };
+            x.Sort();
+            name.Sort();
+
+            //Float list
+            foreach(float i in x)
+            {
+                Console.WriteLine(i);
+            }
+
+            //String list
+            foreach (string i in name)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
