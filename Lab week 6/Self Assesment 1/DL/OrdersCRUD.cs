54330058ccc03d1c8c26f5127a6a5dfc1ec832f1
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-16 19:53:44 +0500

    OOP Tasks

---
+++ Lab week 6/Self Assesment 1/DL/OrdersCRUD.cs
@@ -0,0 +1,39 @@
+﻿using Self_Assesment_1.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assesment_1.DL
+{
+    class OrdersCRUD
+    {
+        
+        public static void RemoveFromList()
+        {
+            o.RemoveAt(0);
+        }
+        public static void AddInList(Orders ord)
+        {
+            o.Add(ord);
+        }
+        public static int PayableAmount()
+        {
+            int total = 0;
+            foreach (MenuItem i in CoffeeShopCRUD.menu)
+            {
+                foreach (Orders j in OrdersCRUD.o)
+                {
+                    if (i.item == j.itemName)
+                    {
+                        total = total + i.price;
+                    }
+                }
+            }
+            return total;
+        }
+
+        public static List<Orders> o = new List<Orders>();
+    }
+}
