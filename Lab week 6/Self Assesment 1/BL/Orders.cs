7faae6f6aaa1b81f231be18280fef0795e3e61c2
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-16 19:50:31 +0500

    OOP Tasks

---
+++ Lab week 6/Self Assesment 1/BL/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assesment_1.BL
+{
+    class Orders
+    {
+        public Orders(string itemName)
+        {
+            this.itemName = itemName;
+        }
+        public string itemName;
+        
+    }
+}
---
+++ Lab week 6/Self Assesment 1/UI/MenuItemUI.cs
@@ -0,0 +1,50 @@
+﻿using Self_Assesment_1.BL;
+using Self_Assesment_1.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assesment_1.UI
+{
+    class MenuItemUI
+    {
+        public void ViewFood()
+        {
+            Console.WriteLine("Item\t\tType\t\tPrice");
+            foreach (MenuItem i in CoffeeShopCRUD.menu)
+            {
+                if (i.type.ToLower() == "food")
+                {
+                    Console.WriteLine(i.item + "\t\t" + i.type + "\t\t" + i.price);
+                }
+            }
+            Console.ReadKey();
+        }
+        public void ViewDrinks()
+        {
+            Console.WriteLine("Item\t\tType\t\tPrice");
+            foreach (MenuItem i in CoffeeShopCRUD.menu)
+            {
+                if (i.type.ToLower() == "drink")
+                {
+                    Console.WriteLine(i.item + "\t\t" + i.type + "\t\t" + i.price);
+                }
+            }
+            Console.ReadKey();
+        }
+        public void ViewCheapestItem(int cheapest)
+        {
+            Console.WriteLine("Item\t\tType\t\tPrice");
+            foreach (MenuItem i in CoffeeShopCRUD.menu)
+            {
+                if (i.price == cheapest)
+                {
+                    Console.WriteLine(i.item + "\t\t" + i.type + "\t\t" + i.price);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
