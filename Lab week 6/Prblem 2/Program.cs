21b382abe359d1398fbc78bc4b0ab31a4bee7b81
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-17 22:36:37 +0500

    OOP Tasks

--- Lab week 6/Prblem 2/Program.cs
+++ Lab week 6/Prblem 2/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Prblem_2.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Prblem_2
@@ -10,6 +12,14 @@ namespace Prblem_2
     {
         static void Main(string[] args)
         {
+            GameObject o = new GameObject();
+            while (true)
+            {
+                Thread.Sleep(100);
+                
+                o.Move();
+            }
+            Console.ReadKey();
         }
     }
 }
