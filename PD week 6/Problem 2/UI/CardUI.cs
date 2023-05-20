48f730b2f8f8ffe6286d8798032071b05db1ba84
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-20 23:59:41 +0500

    OOP Tasks

---
+++ PD week 6/Problem 2/UI/CardUI.cs
@@ -0,0 +1,18 @@
+﻿using Problem_2.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2.UI
+{
+    class CardUI
+    {
+        public static void PrintCard(Card c1)
+        {
+            string card = c1.toString();
+            Console.WriteLine(card);
+        }
+    }
+}
