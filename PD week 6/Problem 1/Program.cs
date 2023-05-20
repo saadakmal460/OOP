b79ac922f51001fc72677104015bda86f8e8e204
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-05-20 13:22:55 +0500

    OOP Tasks

--- PD week 6/Problem 1/Program.cs
+++ PD week 6/Problem 1/Program.cs
@@ -11,17 +11,40 @@ namespace Problem_1
     {
         static void Main(string[] args)
         {
+            bool gameRunning = true;
+
             string path = "maze.txt";
+            
             Grid g = new Grid(path, 24, 71);
+            Pacman p = new Pacman(10, 5, 0, g);
+            Ghost g1 = new Ghost(15, 39, "left", 'R',  0.1F, ' ' , 1F, g);
+            Ghost g2 = new Ghost(1, 1, "left", 'H', 0.1F, ' ', 1F, g);
+            Ghost g3 = new Ghost(10, 15 ,"up", 'V', 0.1F, ' ', 1F, g);
+            List<Ghost> enemies = new List<Ghost>();
+            enemies.Add(g1);
+            enemies.Add(g2);
+            enemies.Add(g3);
+
             g.Draw();
-            Ghost gg = new Ghost(15, 39, "Left", 'R',  0.1F, ' ' , 1F, g);
-            while (true)
+            while (gameRunning)
             {
                 Thread.Sleep(90);
-                gg.Remove();
-                gg.Move();
-                gg.Draw();
-                
+                p.Remove();
+                p.Move();
+                p.Draw();
+                p.PrintScore();
+                foreach(Ghost i in enemies)
+                {
+                    i.Remove();
+                    i.Move();
+                    i.Draw();
+
+                }
+                if(g.IsStoppingCondition())
+                {
+                    gameRunning = false;
+                }
+
             }
         }
     }
