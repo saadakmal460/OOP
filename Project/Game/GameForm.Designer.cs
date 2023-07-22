3ef6c3e5546eaf581751bac36386b6256c3b074d
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-07-22 14:15:14 +0500

    OOP Tasks

--- Project/Game/GameForm.Designer.cs
+++ Project/Game/GameForm.Designer.cs
@@ -32,6 +32,16 @@ namespace Game
             this.components = new System.ComponentModel.Container();
             this.gameLoop = new System.Windows.Forms.Timer(this.components);
             this.enemyTimer = new System.Windows.Forms.Timer(this.components);
+            this.randomEnemyTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbHeart3 = new System.Windows.Forms.PictureBox();
+            this.pbHeart2 = new System.Windows.Forms.PictureBox();
+            this.pbHeart1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart1)).BeginInit();
             this.SuspendLayout();
             // 
             // gameLoop
@@ -42,20 +52,110 @@ namespace Game
             // enemyTimer
             // 
             this.enemyTimer.Enabled = true;
-            this.enemyTimer.Interval = 20000;
+            this.enemyTimer.Interval = 7500;
             this.enemyTimer.Tick += new System.EventHandler(this.enemyTimer_Tick);
+            // 
+            // randomEnemyTimer
+            // 
+            this.randomEnemyTimer.Enabled = true;
+            this.randomEnemyTimer.Interval = 10000;
+            this.randomEnemyTimer.Tick += new System.EventHandler(this.randomEnemyTimer_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.BackColor = System.Drawing.Color.Green;
+            this.progressBar1.Location = new System.Drawing.Point(1520, 167);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(249, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Value = 100;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Orbitron", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(1517, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player Health";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Orbitron", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(1517, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lives";
+            // 
+            // pbHeart3
+            // 
+            this.pbHeart3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbHeart3.BackColor = System.Drawing.Color.Transparent;
+            this.pbHeart3.Image = global::Game.Properties.Resources._removal_ai__a57f3c40_39d6_465a_83fc_9df6986f68e8_heart;
+            this.pbHeart3.Location = new System.Drawing.Point(1648, 254);
+            this.pbHeart3.Name = "pbHeart3";
+            this.pbHeart3.Size = new System.Drawing.Size(58, 38);
+            this.pbHeart3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHeart3.TabIndex = 5;
+            this.pbHeart3.TabStop = false;
+            // 
+            // pbHeart2
+            // 
+            this.pbHeart2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbHeart2.BackColor = System.Drawing.Color.Transparent;
+            this.pbHeart2.Image = global::Game.Properties.Resources._removal_ai__a57f3c40_39d6_465a_83fc_9df6986f68e8_heart;
+            this.pbHeart2.Location = new System.Drawing.Point(1584, 254);
+            this.pbHeart2.Name = "pbHeart2";
+            this.pbHeart2.Size = new System.Drawing.Size(58, 38);
+            this.pbHeart2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHeart2.TabIndex = 4;
+            this.pbHeart2.TabStop = false;
+            // 
+            // pbHeart1
+            // 
+            this.pbHeart1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbHeart1.BackColor = System.Drawing.Color.Transparent;
+            this.pbHeart1.Image = global::Game.Properties.Resources._removal_ai__a57f3c40_39d6_465a_83fc_9df6986f68e8_heart;
+            this.pbHeart1.Location = new System.Drawing.Point(1520, 254);
+            this.pbHeart1.Name = "pbHeart1";
+            this.pbHeart1.Size = new System.Drawing.Size(58, 38);
+            this.pbHeart1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHeart1.TabIndex = 3;
+            this.pbHeart1.TabStop = false;
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkGray;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(21)))));
+            this.BackgroundImage = global::Game.Properties.Resources.blue;
             this.ClientSize = new System.Drawing.Size(1781, 847);
+            this.Controls.Add(this.pbHeart3);
+            this.Controls.Add(this.pbHeart2);
+            this.Controls.Add(this.pbHeart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
             this.DoubleBuffered = true;
             this.Name = "GameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHeart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -63,5 +163,12 @@ namespace Game
 
         private System.Windows.Forms.Timer gameLoop;
         private System.Windows.Forms.Timer enemyTimer;
+        private System.Windows.Forms.Timer randomEnemyTimer;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbHeart1;
+        private System.Windows.Forms.PictureBox pbHeart2;
+        private System.Windows.Forms.PictureBox pbHeart3;
     }
 }
--- Project/Game/GameForm.cs
+++ Project/Game/GameForm.cs
@@ -17,16 +17,68 @@ namespace Game
 
         GameGrid grid;
         GamePLayer player;
+
+
         GameEnemyHorizontal horizontalEnemy1;
         GameEnemyHorizontal horizontalEnemy2;
+        GameEnemyHorizontal horizontalEnemy3;
+        GameEnemyHorizontal horizontalEnemy4;
+        GameEnemyHorizontal horizontalEnemy5;
 
-        GameEnemyRandom r;
-        GameCell fireCell;
-        GameFire f;
-        int playerCounter = 0;
 
+        GameEnemyRandom randomEnemy1;
+        GameEnemyRandom randomEnemy2;
+        GameEnemyRandom randomEnemy3;
+
+        GameEnemyBoss boss;
+
+
+
+        GameCell playerFireCell;
+
+        GameCell randomEnemy1FireCell;
+        GameCell randomEnemy2FireCell;
+        GameCell randomEnemy3FireCell;
+
+        GameCell horizontalEnemy1FireCell;
+        GameCell horizontalEnemy2FireCell;
+        GameCell horizontalEnemy3FireCell;
+        GameCell horizontalEnemy4FireCell;
+        GameCell horizontalEnemy5FireCell;
+
+        GameCell bossFireCell;
+
+
+        GameFire playerFire;
+
+        GameFire randomEnemy1Fire;
+        GameFire randomEnemy2Fire;
+        GameFire randomEnemy3Fire;
+
+        GameFire horizontalEnemy1Fire;
+        GameFire horizontalEnemy2Fire;
+        GameFire horizontalEnemy3Fire;
+        GameFire horizontalEnemy4Fire;
+        GameFire horizontalEnemy5Fire;
+
+        GameFire bossFire;
+
+
+        int playerFireCounter = 0;
+        int extraHorizontalEnemyCounter = 0;
+        int extraRandomEnemyCounter = 0;
+        int randomEnemyFireCount = 0;
+        int horizontalEnemyFireCounter = 0;
+        int bossFireCount = 0;
+        int playerLivesCount = 3;
+
+        bool bossFlag = false;
+
+       
         List<GameEnemy> enemies = new List<GameEnemy>();
         List<GameFire> fire = new List<GameFire>();
+        List<GameFire> enemyFire2 = new List<GameFire>();
+
         public GameForm()
         {
             InitializeComponent();
@@ -34,12 +86,14 @@ namespace Game
 
         private void Game_Load(object sender, EventArgs e)
         {
+            this.WindowState = FormWindowState.Maximized;
             grid = new GameGrid("maze.txt", 12, 17);
+            
             GameCell startCell = new GameCell(6, 3, grid);
             GameCell HorizontalEnemy1cell = new GameCell(2, 3, grid);
             GameCell HorizontalEnemy2cell = new GameCell(5, 3, grid);
-            GameCell enemy2cell = new GameCell(3, 4, grid);
-            GameCell enemy3cell = new GameCell(4, 5, grid);
+            GameCell enemy2cell = new GameCell(2, 3, grid);
+            
 
             Image playerImage = GameClass.getGameObjectImage('p');
             Image enemyImage = GameClass.getGameObjectImage('a');
@@ -47,13 +101,21 @@ namespace Game
             
             player = new GamePLayer(playerImage, startCell);
             horizontalEnemy1 = new GameEnemyHorizontal(enemyImage , HorizontalEnemy1cell, GameObjectType.ENEMY, GameDirection.Left);
-            //horizontalEnemy2 = new GameEnemyHorizontal(enemyImage, HorizontalEnemy2cell, GameObjectType.ENEMY, GameDirection.Right);
+            horizontalEnemy2 = new GameEnemyHorizontal(enemyImage, HorizontalEnemy2cell, GameObjectType.ENEMY, GameDirection.Right);
+            
+            randomEnemy1 = new GameEnemyRandom(enemyImage2, enemy2cell, GameObjectType.ENEMY, GameDirection.Left);
 
-            //r = new GameEnemyRandom(enemyImage2, enemy2cell, GameObjectType.ENEMY, GameDirection.Left);
+            GameCell bossCell = new GameCell(2, 2, grid);
+            Image bossImage = GameClass.getGameObjectImage('b');
+            boss = new GameEnemyBoss(bossImage, bossCell, GameObjectType.ENEMY, GameDirection.Left);
+
 
             enemies.Add(horizontalEnemy1);
-            //enemies.Add(horizontalEnemy2);
-            //enemies.Add(r);
+            enemies.Add(horizontalEnemy2);
+            enemies.Add(randomEnemy1);
+
+            
+
 
             printMaze(grid);
 
@@ -83,70 +145,314 @@ namespace Game
             if (Keyboard.IsKeyPressed(Key.LeftArrow))
             {
                 player.move(GameDirection.Left);
-
+                if (!player.GetFlag() && progressBar1.Value >=20)
+                {
+                    progressBar1.Value -= 20;
+                    player.SetPlayerFlag(true);
+                }
             }
+
             if (Keyboard.IsKeyPressed(Key.RightArrow))
             {
                 player.move(GameDirection.Right);
-
+                if (!player.GetFlag() && progressBar1.Value >= 20)
+                {
+                    progressBar1.Value -= 20;
+                    player.SetPlayerFlag(true);
+                }
             }
+
             if (Keyboard.IsKeyPressed(Key.UpArrow))
             {
                 player.move(GameDirection.Up);
-
+                if (!player.GetFlag() && progressBar1.Value >= 20)
+                {
+                    progressBar1.Value -= 20;
+                    player.SetPlayerFlag(true);
+                }
             }
+
             if (Keyboard.IsKeyPressed(Key.DownArrow))
             {
                 player.move(GameDirection.Down);
-
+                if (!player.GetFlag() && progressBar1.Value >= 20)
+                {
+                    progressBar1.Value -= 20;
+                    player.SetPlayerFlag(true);
+                }
             }
 
-            playerCounter++;
             if(Keyboard.IsKeyPressed(Key.Space))
             {
-                if (playerCounter >= 3)
-                {
-                    fireCell = new GameCell(player.CurrentCell.X, player.CurrentCell.Y, grid);
-                    f = new GameFire(Properties.Resources.bullet2Bg, GameObjectType.Fire, GameDirection.Up, fireCell);
-                    fire.Add(f);
-                    playerCounter = 0;
-                }
+                GeneratePlayerBullet();
+                
 
             }
-            
-            foreach(GameFire i in fire)
+
+            if(progressBar1.Value <= 20 && playerLivesCount == 3)
             {
-                i.MovePlayerFire(grid);
+                pbHeart3.Hide();
+                progressBar1.Value = 100;
+                playerLivesCount--;
+            }
+
+            if (progressBar1.Value <= 20 && playerLivesCount == 2)
+            {
+                pbHeart2.Hide();
+                playerLivesCount--;
+                progressBar1.Value = 100;
+            }
+
+            if (progressBar1.Value <= 20 && playerLivesCount == 1)
+            {
+                pbHeart1.Hide();
+                playerLivesCount--;
+               
+            }
+
+            if (playerLivesCount <= 0)
+            {
+                gameLoop.Enabled = false;
+                MessageBox.Show("You Losse");
+                this.Close();
+            }
+
+
+            //Moving player fires
+            foreach (GameFire i in fire)
+            {
+                i.MovePlayerFire(grid);  
                 if (!i.GetFlag())
                 {
                     fire.Remove(i);
-                    i.CurrentCell.PictureBox.Image = Properties.Resources.simplebox;
+                    i.CurrentCell.PictureBox.Image = Properties.Resources.transparent;
                     break;
                 }
-
                 
             }
 
-            
-            
+            //moving enemies
             foreach (GameEnemy i in enemies)
             {
                 i.MoveGhost(grid);
+                if (!i.GetFlag2())
+                {
+                    enemies.Remove(i);
+                    i.CurrentCell.PictureBox.Image = Properties.Resources.transparent;
+                    break;
+                }
+            }
+
+            //moving enemies bullets
+            foreach (GameFire i in enemyFire2)
+            {
+                i.MoveEnemyFire(grid);
+                if (!i.GetFlag())
+                {
+                    enemyFire2.Remove(i);
+                    i.CurrentCell.PictureBox.Image = Properties.Resources.transparent;
+                    break;
+                }
+            }
+
+            if(enemies.Count == 1 && bossFlag)
+            {
                 
+                enemies.Add(boss);
+            }
+
+            if(!GameClass.GetBossFlag())
+            {
+                gameLoop.Enabled = false;
+                MessageBox.Show("You win");
+                this.Close();
+            }
+
+            
+
+            //generating enemies bullets
+            GenerateRandomEnemyBullet();
+            GenerateHorizontalEnemyBullet();
+            GenerateBossBullets();
+
+            //incrementing counter
+            playerFireCounter++;
+            randomEnemyFireCount++;
+            horizontalEnemyFireCounter++;
+            bossFireCount++;
+        }
+
+
+        
+        private void GenerateHorizontalEnemyBullet()
+        {
+            
+            if (horizontalEnemyFireCounter >= 15)
+            {
+                
+                if (horizontalEnemy1 != null && enemies.Contains(horizontalEnemy1))
+                {
+                    horizontalEnemy1FireCell = new GameCell(horizontalEnemy1.CurrentCell.X, horizontalEnemy1.CurrentCell.Y, grid);
+                    horizontalEnemy1Fire = new GameFire(Properties.Resources.laserRed13, GameObjectType.EnemyFire, horizontalEnemy1FireCell);
+                    enemyFire2.Add(horizontalEnemy1Fire);
+                }
+
+                if (horizontalEnemy2 != null && enemies.Contains(horizontalEnemy2))
+                {
+                    horizontalEnemy2FireCell = new GameCell(horizontalEnemy2.CurrentCell.X, horizontalEnemy2.CurrentCell.Y, grid);
+                    horizontalEnemy2Fire = new GameFire(Properties.Resources.laserRed13, GameObjectType.EnemyFire, horizontalEnemy2FireCell);
+                    enemyFire2.Add(horizontalEnemy2Fire);
+                }
+
+                if (horizontalEnemy3 != null && enemies.Contains(horizontalEnemy3))
+                {
+                    horizontalEnemy3FireCell = new GameCell(horizontalEnemy3.CurrentCell.X, horizontalEnemy3.CurrentCell.Y, grid);
+                    horizontalEnemy3Fire = new GameFire(Properties.Resources.laserRed13, GameObjectType.EnemyFire, horizontalEnemy3FireCell);
+                    enemyFire2.Add(horizontalEnemy3Fire);
+                }
+
+                if (horizontalEnemy4 != null && enemies.Contains(horizontalEnemy4))
+                {
+                    horizontalEnemy4FireCell = new GameCell(horizontalEnemy4.CurrentCell.X, horizontalEnemy4.CurrentCell.Y, grid);
+                    horizontalEnemy4Fire = new GameFire(Properties.Resources.laserRed13, GameObjectType.EnemyFire, horizontalEnemy4FireCell);
+                    enemyFire2.Add(horizontalEnemy4Fire);
+                }
+
+                if (horizontalEnemy5 != null && enemies.Contains(horizontalEnemy5))
+                {
+                    horizontalEnemy5FireCell = new GameCell(horizontalEnemy5.CurrentCell.X, horizontalEnemy5.CurrentCell.Y, grid);
+                    horizontalEnemy5Fire = new GameFire(Properties.Resources.laserRed13, GameObjectType.EnemyFire, horizontalEnemy5FireCell);
+                    enemyFire2.Add(horizontalEnemy5Fire);
+                }
+
+                horizontalEnemyFireCounter = 0;
+            }
+        }
+
+        private void GenerateBossBullets()
+        {
+            if (bossFireCount >= 7)
+            {
+                if (boss != null && enemies.Contains(boss))
+                {
+                    bossFireCell = new GameCell(boss.CurrentCell.X, boss.CurrentCell.Y, grid);
+                    bossFire = new GameFire(Properties.Resources.laserGreen02, GameObjectType.EnemyFire, bossFireCell);
+                    enemyFire2.Add(bossFire);
+
+                }
+                bossFireCount = 0;
+            }
+        }
+        private void GenerateRandomEnemyBullet()
+        {
+            if (randomEnemyFireCount >= 8)
+            {
+                if (randomEnemy1 != null && enemies.Contains(randomEnemy1))
+                {
+                    randomEnemy1FireCell = new GameCell(randomEnemy1.CurrentCell.X, randomEnemy1.CurrentCell.Y, grid);
+                    randomEnemy1Fire = new GameFire(Properties.Resources.laserBlue13, GameObjectType.EnemyFire, randomEnemy1FireCell);
+                    enemyFire2.Add(randomEnemy1Fire);
+
+                }
+                if (randomEnemy2 != null && enemies.Contains(randomEnemy2))
+                {
+                    randomEnemy2FireCell = new GameCell(randomEnemy2.CurrentCell.X, randomEnemy2.CurrentCell.Y, grid);
+                    randomEnemy2Fire = new GameFire(Properties.Resources.laserBlue13, GameObjectType.EnemyFire, randomEnemy2FireCell);
+                    enemyFire2.Add(randomEnemy2Fire);
+
+                }
+                if (randomEnemy3 != null && enemies.Contains(randomEnemy3))
+                {
+                    randomEnemy3FireCell = new GameCell(randomEnemy3.CurrentCell.X, randomEnemy3.CurrentCell.Y, grid);
+                    randomEnemy3Fire = new GameFire(Properties.Resources.laserBlue13, GameObjectType.EnemyFire, randomEnemy3FireCell);
+                    enemyFire2.Add(randomEnemy3Fire);
+
+                }
+                randomEnemyFireCount = 0;
+            }
+        }
+        private void GeneratePlayerBullet()
+        {
+            if (playerFireCounter >= 3)
+            {
+                playerFireCell = new GameCell(player.CurrentCell.X, player.CurrentCell.Y, grid);
+                playerFire = new GameFire(Properties.Resources.bullet2Bg, GameObjectType.Fire, playerFireCell);
+                fire.Add(playerFire);
+                playerFireCounter = 0;
             }
         }
 
         private void enemyTimer_Tick(object sender, EventArgs e)
         {
-            Random rand = new Random();
-            int valueX = rand.Next(1, 6);
-            int valueY = rand.Next(1, 6);
-            GameCell start = new GameCell(valueY, valueX, grid);
-            Image enemyImage = GameClass.getGameObjectImage('a');
-            GameEnemyHorizontal enemy = new GameEnemyHorizontal(enemyImage, start, GameObjectType.ENEMY, GameDirection.Left);
-            enemies.Add(enemy);
+            if (extraHorizontalEnemyCounter < 3)
+            {
+                if (extraHorizontalEnemyCounter == 0)
+                {
+                    Random rand = new Random();
+                    int valueX = rand.Next(1, 6);
+                    int valueY = rand.Next(1, 6);
+                    GameCell start = new GameCell(valueY, valueX, grid);
+                    Image enemyImage = GameClass.getGameObjectImage('a');
+                    horizontalEnemy3 = new GameEnemyHorizontal(enemyImage, start, GameObjectType.ENEMY, GameDirection.Left);
+                    enemies.Add(horizontalEnemy3);
+                }
 
+                if (extraHorizontalEnemyCounter == 1)
+                {
+                    Random rand = new Random();
+                    int valueX = rand.Next(1, 6);
+                    int valueY = rand.Next(1, 6);
+                    GameCell start = new GameCell(valueY, valueX, grid);
+                    Image enemyImage = GameClass.getGameObjectImage('a');
+                    horizontalEnemy4 = new GameEnemyHorizontal(enemyImage, start, GameObjectType.ENEMY, GameDirection.Right);
+                    enemies.Add(horizontalEnemy4);
+                }
 
+                if (extraHorizontalEnemyCounter == 2)
+                {
+                    Random rand = new Random();
+                    int valueX = rand.Next(1, 6);
+                    int valueY = rand.Next(1, 6);
+                    GameCell start = new GameCell(valueY, valueX, grid);
+                    Image enemyImage = GameClass.getGameObjectImage('a');
+                    horizontalEnemy5 = new GameEnemyHorizontal(enemyImage, start, GameObjectType.ENEMY, GameDirection.Left);
+                    enemies.Add(horizontalEnemy5);
+                }
+                extraHorizontalEnemyCounter++;
+            }
+
+        }
+
+        private void randomEnemyTimer_Tick(object sender, EventArgs e)
+        {
+            if (extraRandomEnemyCounter < 2)
+            {
+                if (extraRandomEnemyCounter == 0)
+                {
+                    
+                    Random rand = new Random();
+                    int valueX = rand.Next(1, 6);
+                    int valueY = rand.Next(1, 6);
+                    GameCell start = new GameCell(valueY, valueX, grid);
+                    Image enemyImage = GameClass.getGameObjectImage('c');
+                    randomEnemy2 = new GameEnemyRandom(enemyImage, start, GameObjectType.ENEMY, GameDirection.Left);
+                    enemies.Add(randomEnemy2);
+                }
+
+                if (extraRandomEnemyCounter == 1)
+                {
+                    
+                    Random rand = new Random();
+                    int valueX = rand.Next(1, 6);
+                    int valueY = rand.Next(1, 6);
+                    GameCell start = new GameCell(valueY, valueX, grid);
+                    Image enemyImage = GameClass.getGameObjectImage('c');
+                    randomEnemy3 = new GameEnemyRandom(enemyImage, start, GameObjectType.ENEMY, GameDirection.Right);
+                    enemies.Add(randomEnemy3);
+                    bossFlag = true;
+                }
+                extraRandomEnemyCounter++;
+            }
         }
     }
 }
